f9834f5400348109083de1f7a6d0cfdb84fd7fa3
shivayadavjabbu <92553345+shivayadavjabbu@users.noreply.github.com>
2023-04-24 20:10:54 +0200

    Add files via upload

---
+++ collectstorage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm ./storagecpu
+touch ./storagecpu
+while true; do
+  # Collect flow rules from switches s1, s2, and s3 in Mininet
+  flow_rules_s1=$(sudo ovs-ofctl dump-flows s1 | wc -l)
+  flow_rules_s2=$(sudo ovs-ofctl dump-flows s2 | wc -l)
+  flow_rules_s3=$(sudo ovs-ofctl dump-flows s3 | wc -l)
+
+  # Add the total flow rules to the file "storagecpu"
+  total_flow_rules=$((flow_rules_s1 + flow_rules_s2 + flow_rules_s3))
+
+  total_storage=$( ps aux | grep "root" | grep "503"| grep " ovs-vswitchd unix:/var/run/openvswitch/db.sock"| cut -d ' ' -f 1-20 )
+  echo "$total_flow_rules  $total_storage " >> ./storagecpu
+
+  # Sleep for 30 seconds before collecting the flow rules again
+  sleep 60
+done
